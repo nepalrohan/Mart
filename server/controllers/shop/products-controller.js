d83ce7af49ac3f2196dc7d860f27431a0cfc307d
nepalrohan <rohannepal098@gmail.com>
2025-03-10 20:22:18 +0545

    add cart controller

--- server/controllers/shop/products-controller.js
+++ server/controllers/shop/products-controller.js
@@ -46,7 +46,7 @@ const getFilteredProducts = async (req, res) => {
       success: true,
       data: products,
     });
-  } catch (e) {
+  } catch (error) {
     console.log(error);
     res.status(500).json({
       success: false,
@@ -55,4 +55,38 @@ const getFilteredProducts = async (req, res) => {
   }
 };
 
-export { getFilteredProducts };
+
+
+
+const getProductDetails = async (req, res)=>{
+  try {
+    
+    const {id} = req.params;
+
+    const product = await Product.findById(id);
+    if(!product){
+      return res.status(404).json({
+        success:false,
+        message:'Product not found'
+      })
+    }
+
+
+    return res.status(200).json({
+      success:true,
+      data:product
+    })
+
+    
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
+    });
+  }
+}
+
+
+export { getFilteredProducts, getProductDetails };
